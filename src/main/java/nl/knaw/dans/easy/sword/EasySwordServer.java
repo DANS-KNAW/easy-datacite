5cb6e2de415bdc6503d1c67ab1e0e5c57f0d5056
Joke Pol <joke.pol@dans.knaw.nl>
2011-04-15 14:41:26 +0000

    removed some obsolete javadoc
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/easy-sword/trunk@8739 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/main/java/nl/knaw/dans/easy/sword/EasySwordServer.java
+++ src/main/java/nl/knaw/dans/easy/sword/EasySwordServer.java
@@ -1,22 +1,3 @@
-/**
- * Copyright (c) 2009, Aberystwyth University All rights reserved. Redistribution and use in source and
- * binary forms, with or without modification, are permitted provided that the following conditions are
- * met: - Redistributions of source code must retain the above copyright notice, this list of conditions
- * and the following disclaimer. - Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the documentation and/or other
- * materials provided with the distribution. - Neither the name of the Centre for Advanced Software and
- * Intelligent Systems (CASIS) nor the names of its contributors may be used to endorse or promote
- * products derived from this software without specific prior written permission. THIS SOFTWARE IS
- * PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package nl.knaw.dans.easy.sword;
 
 import java.io.File;
@@ -58,21 +39,10 @@ import org.purl.sword.server.SWORDServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * A 'dummy server' which acts as dumb repository which implements the SWORD ServerInterface. It accepts
- * any type of deposit, and tries to return appropriate responses. It supports authentication: if the
- * username and password match (case sensitive) it authenticates the user, if not, the authentication
- * fails.
- * 
- * @author Stuart Lewis
- * @author J. Pol (extracted some repeated code into methods, added EASY authentication)
- */
 public class EasySwordServer implements SWORDServer
 {
-    /** A counter to count submissions, so the response to a deposit can increment */
-    private static int    counter = 0;
+    private static int    sumbitCounter = 0;
 
-    /** Logger */
     private static Logger log     = LoggerFactory.getLogger(EasySwordServer.class);
 
     /**
@@ -217,7 +187,7 @@ public class EasySwordServer implements SWORDServer
         {
             // Handle the deposit
             unzipped.submit(user);
-            counter++;
+            sumbitCounter++;
         }
         final Summary wrapSummary = wrapSummary(deposit.getFilename(), deposit.getSlug(), unzipped.getFiles());
         return wrapResponse(wrapSwordEntry(deposit, wrapSummary));
@@ -251,14 +221,14 @@ public class EasySwordServer implements SWORDServer
     {
         final DepositResponse depostiResponse = new DepositResponse(Deposit.CREATED);
         depostiResponse.setEntry(swordEntry);
-        depostiResponse.setLocation("http://www.myrepository.ac.uk/atom/" + counter);
+        depostiResponse.setLocation("http://www.myrepository.ac.uk/atom/" + sumbitCounter);
         return depostiResponse;
     }
 
     private static Title wrapTitle()
     {
         final Title title = new Title();
-        title.setContent("DummyServer Deposit: #" + counter);
+        title.setContent("DummyServer Deposit: #" + sumbitCounter);
         return title;
     }
 
@@ -267,11 +237,11 @@ public class EasySwordServer implements SWORDServer
         final String id;
         if (slug != null)
         {
-            id = slug + " - ID: " + counter;
+            id = slug + " - ID: " + sumbitCounter;
         }
         else
         {
-            id = "ID: " + counter;
+            id = "ID: " + sumbitCounter;
         }
         return id;
     }
@@ -333,7 +303,7 @@ public class EasySwordServer implements SWORDServer
         {
             log.error(mediaType,exception);
         }
-        content.setSource("http://www.myrepository.ac.uk/sdl/uploads/upload-" + counter + ".zip");
+        content.setSource("http://www.myrepository.ac.uk/sdl/uploads/upload-" + sumbitCounter + ".zip");
         return content;
     }
 
