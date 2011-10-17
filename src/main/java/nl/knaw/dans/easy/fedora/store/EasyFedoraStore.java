464a2b265603c5381a22ae1c15ba7e8a4240c2f5
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2011-10-17 12:15:25 +0000

    - universele methode voor datastream url
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof/branches/2.0/easy/easy-application/easy-fedora@9765 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/main/java/nl/knaw/dans/easy/fedora/store/EasyFedoraStore.java
+++ src/main/java/nl/knaw/dans/easy/fedora/store/EasyFedoraStore.java
@@ -14,7 +14,6 @@ import nl.knaw.dans.common.jibx.JiBXObjectFactory;
 import nl.knaw.dans.common.lang.RepositoryException;
 import nl.knaw.dans.common.lang.repo.DataModelObject;
 import nl.knaw.dans.common.lang.repo.exception.ObjectDeserializationException;
-import nl.knaw.dans.common.lang.repo.jumpoff.JumpoffDmo;
 import nl.knaw.dans.common.lang.repo.relations.RelsConstants;
 import nl.knaw.dans.common.lang.reposearch.RepoSearchListener;
 import nl.knaw.dans.common.lang.search.SearchEngine;
@@ -63,10 +62,16 @@ public class EasyFedoraStore extends FedoraDmoStore implements EasyStore
 
     }
     
-    public URL getFileURL(String sid)
+    public URL getFileURL(String storeId)
+    {
+        return getStreamURL(storeId, EasyFile.UNIT_ID);
+    }
+    
+    @Override
+    public URL getStreamURL(String storeId, String streamId)
     {
         URL url = null;
-        String spec = getFedora().getBaseURL() + "/get/" + sid + "/" + EasyFile.UNIT_ID;
+        final String spec = getFedora().getBaseURL() + "/get/" + storeId + "/" + streamId;
         try
         {
             url = new URL(spec);
@@ -76,12 +81,11 @@ public class EasyFedoraStore extends FedoraDmoStore implements EasyStore
             throw new ApplicationException(e);
         }
         return url;
-        
     }
 
-    public URL getDescriptiveMetadataURL(String sid)
+    public URL getDescriptiveMetadataURL(String storeId)
     {
-        final String spec = getFedora().getBaseURL() + "/get/" + sid + "/" + DescriptiveMetadataImpl.UNIT_ID;
+        final String spec = getFedora().getBaseURL() + "/get/" + storeId + "/" + DescriptiveMetadataImpl.UNIT_ID;
         try
         {
             return new URL(spec);
@@ -152,9 +156,9 @@ public class EasyFedoraStore extends FedoraDmoStore implements EasyStore
         return dlhList;
     }
 
-    public EasyMetadata getEasyMetaData(String sid, DateTime asOfDateTime) throws RepositoryException
+    public EasyMetadata getEasyMetaData(String storeId, DateTime asOfDateTime) throws RepositoryException
     {
-        final MIMETypedStream mts = getFedora().getDatastreamAccessor().getDatastreamDissemination(sid, EasyMetadata.UNIT_ID, asOfDateTime);
+        final MIMETypedStream mts = getFedora().getDatastreamAccessor().getDatastreamDissemination(storeId, EasyMetadata.UNIT_ID, asOfDateTime);
         try
         {
             return (EasyMetadata) JiBXObjectFactory.unmarshal(EasyMetadataImpl.class, mts.getStream());
