90a3b2df913b136490a696886eac0dc6ab487775
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2011-10-17 13:01:13 +0000

    - refactoring
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof-open-source/branches/2.0/dans-ldap@9768 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/test/java/nl/knaw/dans/common/ldap/management/ApacheDSServerBuilderOnlineTest.java
+++ src/test/java/nl/knaw/dans/common/ldap/management/ApacheDSServerBuilderOnlineTest.java
@@ -1,10 +1,12 @@
 package nl.knaw.dans.common.ldap.management;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ApacheDSServerBuilderOnlineTest
 {
     
+    @Ignore("Not a test")
     @Test
     public void buildApacheDS() throws Exception
     {
