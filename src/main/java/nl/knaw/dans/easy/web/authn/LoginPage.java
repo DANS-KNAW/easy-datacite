ad983ba4d4f9d3c07c96e20f268cf63c17e6a26c
Paul Boon <paul.boon@dans.knaw.nl>
2012-02-01 10:37:09 +0000

    support federative login
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof/branches/2.0/easy/easy-application/easy-webui@10300 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/main/java/nl/knaw/dans/easy/web/authn/LoginPage.java
+++ src/main/java/nl/knaw/dans/easy/web/authn/LoginPage.java
@@ -1,5 +1,8 @@
 package nl.knaw.dans.easy.web.authn;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import nl.knaw.dans.common.lang.service.exceptions.ServiceException;
 import nl.knaw.dans.common.wicket.exceptions.InternalWebError;
 import nl.knaw.dans.easy.domain.authn.UsernamePasswordAuthentication;
@@ -7,6 +10,8 @@ import nl.knaw.dans.easy.servicelayer.services.Services;
 import nl.knaw.dans.easy.web.EasyResources;
 
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.protocol.https.RequireHttps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +40,10 @@ public class LoginPage extends AbstractAuthenticationPage
     private void init()
     {
         setStatelessHint(true);
+        
+        // Federative Athentication Link
+        add(new ExternalLink("federationLink", getFederationURLString(), "Login"));
+        
         UsernamePasswordAuthentication authentication;
 		try
 		{
@@ -49,6 +58,35 @@ public class LoginPage extends AbstractAuthenticationPage
         this.add(new LoginPanel(LOGIN_PANEL_BIG, authentication));
     }
 
+    // construct the link for the Federative Athentication
+    private String getFederationURLString()
+    {
+      String linkURLString = "";
+      
+      try
+      {
+          // get URL for FederativeAuthenticationResultPage
+          String relStr = RequestCycle.get().urlFor(FederativeAuthenticationResultPage.class, new PageParameters()).toString();
+          String returnURLString = org.apache.wicket.protocol.http.RequestUtils.toAbsolutePath(relStr);
+          logger.debug("return URL: " + returnURLString);
+
+          returnURLString = URLEncoder.encode(returnURLString, "UTF-8");
+          
+          // add the easy return page url as parameter to 
+          // the external federation service provider (SP) url
+          String federationURLString = "https://eof12.dans.knaw.nl/Shibboleth.sso/Login?target=";
+          linkURLString = federationURLString + returnURLString;
+          logger.debug("link URL: " + linkURLString);
+      }
+      catch (UnsupportedEncodingException e)
+      {
+          logger.error("Could not construct Federative login link", e);
+      }
+      
+
+      return linkURLString;
+    }
+    
     /**
      * Default constructor.
      */
