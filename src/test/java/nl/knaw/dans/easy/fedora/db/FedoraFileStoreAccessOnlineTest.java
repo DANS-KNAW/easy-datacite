d47a22a2ba8244cd360aca9f09e12cee5bc49567
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2012-02-16 15:09:53 +0000

    - not saving folders
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof/branches/2.0/easy/easy-application/easy-fedora@10386 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/test/java/nl/knaw/dans/easy/fedora/db/FedoraFileStoreAccessOnlineTest.java
+++ src/test/java/nl/knaw/dans/easy/fedora/db/FedoraFileStoreAccessOnlineTest.java
@@ -33,7 +33,7 @@ public class FedoraFileStoreAccessOnlineTest extends AbstractOnlineTest
     @Test
     public void findById() throws StoreAccessException
     {
-        String fileId = "easy-file:2071";
+        String fileId = "easy-file:3";
         FileItemVO fivo = fsAccess.findFileById(fileId);
         assertEquals(fileId, fivo.getSid());
     }
@@ -42,20 +42,20 @@ public class FedoraFileStoreAccessOnlineTest extends AbstractOnlineTest
     @Test
     public void findByIdList() throws StoreAccessException
     {
-        List<String> sids = Arrays.asList(new String[] {"easy-file:207", "easy-file:2173", "easy-file:3695"});
+        List<String> sids = Arrays.asList(new String[] {"easy-file:2", "easy-file:3", "easy-file:not-5"});
         
         List<FileItemVO> fivos = fsAccess.findFilesById(sids);
         assertEquals(2, fivos.size());
-        assertEquals("easy-file:2173", fivos.get(0).getSid());
-        assertEquals("easy-file:3695", fivos.get(1).getSid());
+        assertEquals("easy-file:2", fivos.get(0).getSid());
+        assertEquals("easy-file:3", fivos.get(1).getSid());
     }
     
     @Ignore("Local variables")
     @Test 
     public void findFileByPath() throws Exception
     {
-        String datasetId = "easy-dataset:10";
-        String relativePath = "original/img/00000087.002";
+        String datasetId = "easy-dataset:1";
+        String relativePath = "opdracht-4_exercise-3/images/iNav-ext-feature-tree.png";
         FileItemVO fivo = fsAccess.findFileByPath(datasetId, relativePath);
         assertEquals(relativePath, fivo.getPath());
         assertEquals(datasetId, fivo.getDatasetSid());
@@ -89,18 +89,24 @@ public class FedoraFileStoreAccessOnlineTest extends AbstractOnlineTest
     @Test
     public void canItFindDatasetChildren() throws StoreAccessException
     {
-        List<ItemVO> datasetKids = fsAccess.getFilesAndFolders("easy-dataset:721", -1, -1, null, null);
+        List<ItemVO> datasetKids = fsAccess.getFilesAndFolders("easy-dataset:1", -1, -1, null, null);
         for (ItemVO kid : datasetKids)
+        {
+            System.out.println(kid.getName());
+        }
+        
+        List<FileItemVO> kids = fsAccess.getFiles("easy-dataset:1", -1, -1, null, null);
+        for (ItemVO kid : kids)
         {
             System.out.println(kid.getName());
         }
     }
     
-    //@Ignore("Local variables")
+    @Ignore("Local variables")
     @Test
     public void getFileNames() throws StoreAccessException
     {
-        List<String> filenames = fsAccess.getFilenames("easy-dataset:201", true);
+        List<String> filenames = fsAccess.getFilenames("easy-dataset:1", true);
         for (String s : filenames)
         {
             System.err.println(s);
@@ -125,6 +131,21 @@ public class FedoraFileStoreAccessOnlineTest extends AbstractOnlineTest
     public void getDatasetIdWithWrongParameter() throws Exception
     {
         fsAccess.getDatasetId("easy-dataset:1");
+    }
+    
+    @Ignore("Local variables")
+    @Test
+    public void getItemAndAllChildren() throws Exception
+    {
+        List<ItemVO> items = fsAccess.getItemAndAllChildren("easy-dataset:1");
+        System.err.println(items.size());
+        for (ItemVO item : items)
+        {
+            System.err.println(item + " " + item.getSid());
+        }
+        
+        List<ItemVO> datasetKids = fsAccess.getFilesAndFolders("easy-dataset:1", -1, -1, null, null);
+        System.err.println(datasetKids.size());
     }
 
 }
