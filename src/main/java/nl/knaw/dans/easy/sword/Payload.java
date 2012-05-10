fbda34219501710d09c75da543ee6ee53320df89
Joke Pol <joke.pol@dans.knaw.nl>
2012-05-10 08:00:48 +0000

    refactoreds constructor for readabillity
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/easy-sword/trunk@10905 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/main/java/nl/knaw/dans/easy/sword/Payload.java
+++ src/main/java/nl/knaw/dans/easy/sword/Payload.java
@@ -21,47 +21,45 @@ import org.slf4j.LoggerFactory;
 
 public class Payload
 {
-    private static Logger    log = LoggerFactory.getLogger(EasyBusinessFacade.class);
+    private static Logger      log = LoggerFactory.getLogger(EasyBusinessFacade.class);
 
-    private final List<File> files;
-    private final File       tempDir;
-    private final File       dataFolder;
-    private final File       metadataFile;
+    private final List<File>   files;
+    private final File         tempDir;
+    private final File         dataFolder;
+    private final File         metadataFile;
+
+    private final EasyMetadata easyMetadata;
 
     public Payload(final InputStream inputStream) throws SWORDException, SWORDErrorException
     {
+        tempDir = createTempDir();
+        files = unzip(inputStream);
+
+        final File[] rootEntries = zipHasTwoRootEntries();
+        if (rootEntries[0].isDirectory() && rootEntries[1].isFile())
+        {
+            dataFolder = rootEntries[0];
+            metadataFile = rootEntries[1];
+        }
+        else if (rootEntries[1].isDirectory() && rootEntries[0].isFile())
+        {
+            dataFolder = rootEntries[1];
+            metadataFile = rootEntries[0];
+        }
+        else
+        {
+            throw newSwordInputException("Expecting a file and a folder with files.", null);
+        }
+
+        folderHasFiles();
+        easyMetadata = createEasyMetadata();
+    }
+
+    private List<File> unzip(final InputStream inputStream) throws SWORDErrorException
+    {
         try
         {
-            tempDir = FileUtil.createTempDirectory(new File(Context.getUnzip()), "swunzip");
-            files = UnzipUtil.unzip(new ZipInputStream(new BufferedInputStream(inputStream)), tempDir.getPath());
-            final File[] rootContent = tempDir.listFiles();
-            if (files.size() < 2 || rootContent == null || rootContent.length != 2)
-            {
-                // an XML file and a folder in the root of the unzipped directory, no more no less
-                throw newSwordInputException("Expecting a file and a folder with files.",null);
-            }
-            if (rootContent[0].isDirectory() && rootContent[1].isFile())
-            {
-                dataFolder = rootContent[0];
-                metadataFile = rootContent[1];
-            }
-            else if (rootContent[1].isDirectory() && rootContent[0].isFile())
-            {
-                dataFolder = rootContent[1];
-                metadataFile = rootContent[0];
-            }
-            else
-            {
-                throw newSwordInputException("Expecting a file and a folder with files.",null);
-            }
-            for (final File file : files)
-            {
-                // yes, we do have some real data
-                if (file.isFile() && !file.getPath().equals(metadataFile.toString()))
-                    return;
-            }
-            // oops, just folders
-            throw new SWORDErrorException(ErrorCodes.ERROR_BAD_REQUEST, "No files in the data folder.");
+            return UnzipUtil.unzip(new ZipInputStream(new BufferedInputStream(inputStream)), tempDir.getPath());
         }
         catch (final ZipException exception)
         {
@@ -71,6 +69,47 @@ public class Payload
         {
             throw newSwordInputException("Failed to unzip deposited file", exception);
         }
+    }
+
+    private File createTempDir() throws SWORDException
+    {
+        try
+        {
+            return FileUtil.createTempDirectory(new File(Context.getUnzip()), "swunzip");
+        }
+        catch (final IOException exception)
+        {
+            throw newSWORDException("Could not create temp dir for unzip", exception);
+        }
+    }
+
+    private File[] zipHasTwoRootEntries() throws SWORDErrorException
+    {
+        final File[] rootContent = tempDir.listFiles();
+        if (files.size() < 2 || rootContent == null || rootContent.length != 2)
+        {
+            // an XML file and a folder in the root of the unzipped directory, no more no less
+            throw newSwordInputException("Expecting a file and a folder with files.", null);
+        }
+        return rootContent;
+    }
+
+    private EasyMetadata createEasyMetadata() throws SWORDErrorException, SWORDException
+    {
+        if (metadataFile.getName().equals("easyMetadata.xml"))
+            return EasyMetadataFacade.validate(readMetadata());
+        else
+            return EasyMetadataFacade.validate(readMetadata());
+    }
+
+    private void folderHasFiles() throws SWORDErrorException
+    {
+        for (final File file : files)
+        {
+            if (file.isFile() && !file.getPath().equals(metadataFile.toString()))
+                return;
+        }
+        throw newSwordInputException("No files in the folder.", null);
     }
 
     public List<File> getFiles()
@@ -88,9 +127,15 @@ public class Payload
 
     public EasyMetadata getEasyMetadata() throws SWORDException, SWORDErrorException
     {
+        return easyMetadata;
+    }
+
+    private byte[] readMetadata() throws SWORDErrorException, SWORDException
+    {
+        byte[] readFile;
         try
         {
-            return EasyMetadataFacade.validate(FileUtil.readFile(metadataFile));
+            readFile = FileUtil.readFile(metadataFile);
         }
         catch (final FileNotFoundException exception)
         {
@@ -101,6 +146,7 @@ public class Payload
         {
             throw newSWORDException("Failed to extract the EasyMetadata", exception);
         }
+        return readFile;
     }
 
     private static SWORDException newSWORDException(final String message, final Exception exception)
