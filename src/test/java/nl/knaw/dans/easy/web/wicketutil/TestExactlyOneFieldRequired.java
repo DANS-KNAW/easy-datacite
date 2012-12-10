e834d410c399b3634f042c33295148006005b6ed
Vesa Akerman <vesa.akerman@dans.knaw.nl>
2012-12-10 11:47:10 +0000

    https://drivenbydata.onjira.com/browse/EASY-507
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof/branches/2.0/easy/easy-application/easy-webui@12004 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

---
+++ src/test/java/nl/knaw/dans/easy/web/wicketutil/TestExactlyOneFieldRequired.java
@@ -0,0 +1,54 @@
+package nl.knaw.dans.easy.web.wicketutil;
+
+import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertTrue;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import nl.knaw.dans.common.wicket.util.RequireExactlyOneValidator;
+
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( {RequireExactlyOneValidator.class, Form.class, FormComponent.class})
+public class TestExactlyOneFieldRequired
+{
+    private boolean errorCalled = false;
+
+    @Test
+    public void twoComponentsOneFilledInIsCorrect() throws Exception
+    {
+        FormComponent comp1 = PowerMock.createMock(FormComponent.class);
+        FormComponent comp2 = PowerMock.createMock(FormComponent.class);
+        expect(comp1.getValue()).andReturn("kalle").anyTimes();
+        expect(comp2.getValue()).andReturn("").anyTimes();
+        Form form = PowerMock.createMock(Form.class);
+        replayAll();
+        RequireExactlyOneValidator requireExactlyOneValidator = new RequireExactlyOneValidator(new FormComponent[] {comp1, comp2});
+        requireExactlyOneValidator.validate(form);
+    }
+
+    @Test
+    public void twoComponentsTwoFilledInIsNotCorrect() throws Exception
+    {
+        FormComponent comp1 = PowerMock.createMock(FormComponent.class);
+        FormComponent comp2 = PowerMock.createMock(FormComponent.class);
+        expect(comp1.getValue()).andReturn("kalle").anyTimes();
+        expect(comp2.getValue()).andReturn("kalle.kivi@kallio.com").anyTimes();
+        Form form = PowerMock.createMock(Form.class);
+        replayAll();
+        RequireExactlyOneValidator requireExactlyOneValidator = new RequireExactlyOneValidator(comp1, comp2)
+        {
+            public void error(org.apache.wicket.markup.html.form.FormComponent<?> fc)
+            {
+                errorCalled = true;
+            };
+        };
+        requireExactlyOneValidator.validate(form);
+        assertTrue("Validator did not detect that too many fields were filled in", errorCalled);
+    }
+}
