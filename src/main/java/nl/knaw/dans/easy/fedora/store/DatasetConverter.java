029ec73cbec5de940d39ff3303169ae3320b9235
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2012-12-14 14:52:25 +0000

    EASY-576 Lijst van gepubliceerde datasets zonder bestanden
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof/branches/2.0/easy/easy-application/easy-fedora@12030 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/main/java/nl/knaw/dans/easy/fedora/store/DatasetConverter.java
+++ src/main/java/nl/knaw/dans/easy/fedora/store/DatasetConverter.java
@@ -84,59 +84,64 @@ public class DatasetConverter extends AbstractDobConverter<DatasetImpl>
     {
         super.deserialize(digitalObject, dataset);
 
+        DatastreamVersion emdVersion = digitalObject.getLatestVersion(EasyMetadata.UNIT_ID);
+        if (emdVersion != null)
+        {
+            Element element = emdVersion.getXmlContentElement();
+            EasyMetadata emd = (EasyMetadata) unmarshal(EasyMetadataImpl.class, element);
+            emd.setTimestamp(emdVersion.getTimestamp());
+            emd.setDirty(false);
+            dataset.setEasyMetadata(emd);
+        }
+        else
+        {
+            logger.warn("No easyMetadata found on retrieved digital object. sid=" + digitalObject.getSid());
+        }
+
+        DatastreamVersion amdVersion = digitalObject.getLatestVersion(AdministrativeMetadata.UNIT_ID);
+        if (amdVersion != null)
+        {
+            Element element = amdVersion.getXmlContentElement();
+            AdministrativeMetadata amd = (AdministrativeMetadata) unmarshal(AdministrativeMetadataImpl.class, element);
+            amd.setTimestamp(amdVersion.getTimestamp());
+            amd.setDirty(false);
+            dataset.setAdministrativeMetadata(amd);
+        }
+        else
+        {
+            logger.warn("No administrative metadata found on retrieved digital object. sid=" + digitalObject.getSid());
+        }
+
+        DatastreamVersion icmdVersion = digitalObject.getLatestVersion(DatasetItemContainerMetadata.UNIT_ID);
+        if (icmdVersion != null)
+        {
+            Element element = icmdVersion.getXmlContentElement();
+            ItemContainerMetadataImpl icmd = (ItemContainerMetadataImpl) unmarshal(ItemContainerMetadataImpl.class, element);
+            icmd.setDirty(false);
+            icmd.setTimestamp(icmdVersion.getTimestamp());
+            dataset.setItemContainerMetadata(icmd);
+        }
+
+        DatastreamVersion pslVersion = digitalObject.getLatestVersion(PermissionSequenceList.UNIT_ID);
+        if (pslVersion != null)
+        {
+            Element element = pslVersion.getXmlContentElement();
+            PermissionSequenceList psl = (PermissionSequenceListImpl) unmarshal(PermissionSequenceListImpl.class, element);
+            psl.setDirty(false);
+            psl.setTimestamp(pslVersion.getTimestamp());
+            dataset.setPermissionSequenceList(psl);
+        }
+    }
+
+    private Object unmarshal(Class<?> classToInstantiate, Element rootElement) throws ObjectDeserializationException
+    {
         try
         {
-            DatastreamVersion emdVersion = digitalObject.getLatestVersion(EasyMetadata.UNIT_ID);
-            if (emdVersion != null)
-            {
-                Element element = emdVersion.getXmlContentElement();
-                EasyMetadata emd = (EasyMetadata) JiBXObjectFactory.unmarshal(EasyMetadataImpl.class, element);
-                emd.setTimestamp(emdVersion.getTimestamp());
-                emd.setDirty(false);
-                dataset.setEasyMetadata(emd);
-            }
-            else
-            {
-                logger.warn("No easyMetadata found on retrieved digital object. sid=" + digitalObject.getSid());
-            }
-
-            DatastreamVersion amdVersion = digitalObject.getLatestVersion(AdministrativeMetadata.UNIT_ID);
-            if (amdVersion != null)
-            {
-                Element element = amdVersion.getXmlContentElement();
-                AdministrativeMetadata amd = (AdministrativeMetadata) JiBXObjectFactory.unmarshal(AdministrativeMetadataImpl.class, element);
-                amd.setTimestamp(amdVersion.getTimestamp());
-                amd.setDirty(false);
-                dataset.setAdministrativeMetadata(amd);
-            }
-            else
-            {
-                logger.warn("No administrative metadata found on retrieved digital object. sid=" + digitalObject.getSid());
-            }
-
-            DatastreamVersion icmdVersion = digitalObject.getLatestVersion(DatasetItemContainerMetadata.UNIT_ID);
-            if (icmdVersion != null)
-            {
-                Element element = icmdVersion.getXmlContentElement();
-                ItemContainerMetadataImpl icmd = (ItemContainerMetadataImpl) JiBXObjectFactory.unmarshal(ItemContainerMetadataImpl.class, element);
-                icmd.setDirty(false);
-                icmd.setTimestamp(icmdVersion.getTimestamp());
-                dataset.setItemContainerMetadata(icmd);
-            }
-
-            DatastreamVersion pslVersion = digitalObject.getLatestVersion(PermissionSequenceList.UNIT_ID);
-            if (pslVersion != null)
-            {
-                Element element = pslVersion.getXmlContentElement();
-                PermissionSequenceList psl = (PermissionSequenceListImpl) JiBXObjectFactory.unmarshal(PermissionSequenceListImpl.class, element);
-                psl.setDirty(false);
-                psl.setTimestamp(pslVersion.getTimestamp());
-                dataset.setPermissionSequenceList(psl);
-            }
-
+            return JiBXObjectFactory.unmarshal(classToInstantiate, rootElement);
         }
         catch (XMLDeserializationException e)
         {
+            logger.error("Could not unmarshall element {} into an instance of {}", rootElement, classToInstantiate);
             throw new ObjectDeserializationException(e);
         }
     }
