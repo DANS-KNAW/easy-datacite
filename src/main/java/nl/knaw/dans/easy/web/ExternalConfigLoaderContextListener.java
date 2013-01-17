7df9ace9d4612ce102cba247513eebe58166ffa6
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2013-01-17 12:28:34 +0000

    Comment toegevoegd over het waarom van deze DRY-violation.
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof/branches/2.0/easy/easy-application/easy-webui@12112 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/main/java/nl/knaw/dans/easy/web/ExternalConfigLoaderContextListener.java
+++ src/main/java/nl/knaw/dans/easy/web/ExternalConfigLoaderContextListener.java
@@ -20,6 +20,15 @@ public class ExternalConfigLoaderContextListener implements ServletContextListen
         try
         {
             log.debug("Creating temporary homedirectory bean, because Spring initialization hasn't run yet");
+            /*
+             * This is a DRY-violation, because the name of the environment variable pointing to the home
+             * directory, EASY_WEBUI_HOME, is also defined in applicationContext.xml. However, we want to
+             * configure logging as soon as possible, i.e. before Spring initializes. Otherwise we won't
+             * be able to configure how Spring initialization is logged in
+             * $EASY_WEBUI_HOME/cfg/logback.xml. Unless you see a better way to do this (and that doesn't
+             * make it necessary to configure logging in multiple places), please leave this small
+             * DRY-violation in place.
+             */
             EnvironmentVariableBasedHomeDirectory home = new EnvironmentVariableBasedHomeDirectory();
             home.setEnvironmentVariableName("EASY_WEBUI_HOME");
             new LogBackConfigLoader(new File(home.getHomeDirectory(), "cfg/logback.xml"));
