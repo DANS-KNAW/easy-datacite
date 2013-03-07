451237f6706b23e304ed7d2c3acdb72a211880d7
Henk van den Berg <henk.van.den.berg@dans.knaw.nl>
2013-03-07 15:58:49 +0000

    - add additional RDF during ingest of fileItems
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/eof/branches/2.0/easy/easy-application/easy-business@12223 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

---
+++ src/main/java/nl/knaw/dans/easy/domain/model/FileOntology.java
@@ -0,0 +1,47 @@
+package nl.knaw.dans.easy.domain.model;
+
+/**
+ * Voorlopige (en zeer beperkte) ontologie voor files.
+ * 
+ * @author henk van den berg
+ */
+public class FileOntology
+{
+
+    public enum MetadataFormat
+    {
+        CMDI, DDM, DDI 
+    }
+    
+    public static final String URI = "http://easy.dans.knaw.nl/ontologies/file#";
+    public static final String PREFIX = "easy-file";
+
+    public final String IS_METADATA_ON = "isMetadataOn";
+
+    public final String HAS_METADATA_FORMAT = "hasMetadataFormat";
+
+    private String text = URI;
+
+    public String get()
+    {
+        return text;
+    }
+
+    public String iri()
+    {
+        return "<" + text + ">";
+    }
+
+    public FileOntology isMetadataOn()
+    {
+        text = URI + IS_METADATA_ON;
+        return this;
+    }
+
+    public FileOntology hasMetadataFormat()
+    {
+        text = URI + HAS_METADATA_FORMAT;
+        return this;
+    }
+
+}
