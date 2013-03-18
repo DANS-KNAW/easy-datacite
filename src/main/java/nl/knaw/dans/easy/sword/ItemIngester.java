b30a3fbaeafc6a9bdc280faba0ce32e718ea6ffb
Joke Pol <joke.pol@dans.knaw.nl>
2013-03-18 15:19:51 +0000

    fixed issue EASY-652
    git-svn-id: svn+ssh://develop01.dans.knaw.nl/development/svn/repos/mixed/easy-sword/trunk@12234 a75a9a65-0d41-4860-8c96-5fae3ec79aa9

--- src/main/java/nl/knaw/dans/easy/sword/ItemIngester.java
+++ src/main/java/nl/knaw/dans/easy/sword/ItemIngester.java
@@ -2,38 +2,43 @@ package nl.knaw.dans.easy.sword;
 
 import nl.knaw.dans.common.lang.repo.relations.AbstractRelations;
 import nl.knaw.dans.common.lang.repo.relations.RelsConstants;
+import nl.knaw.dans.easy.business.item.ItemIngester.DefaultDelegator;
 import nl.knaw.dans.easy.domain.model.Dataset;
 import nl.knaw.dans.easy.domain.model.FileItem;
 import nl.knaw.dans.easy.domain.model.FileOntology;
-import nl.knaw.dans.easy.business.item.ItemIngester.DefaultDelegator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ItemIngester extends DefaultDelegator
 {
-    
-    private Dataset dataset;
+    private static final String DDM = "original/" + RequestContent.MDFileName.DansDatasetMetadata;
+    private static final String EMD = "original/" + RequestContent.MDFileName.easyMetadata;
+    private static final String FO_DDM = FileOntology.MetadataFormat.DDM.toString();
+    private static Logger logger = LoggerFactory.getLogger(ItemIngester.class);
+    private final Dataset dataset;
 
-    public ItemIngester(Dataset dataset)
+    public ItemIngester(final Dataset dataset)
     {
         super(dataset);
         this.dataset = dataset;
     }
 
     @Override
-    public void addAdditionalRDF(final FileItem fileItem){
-        if (fileItem!=null)return;
-        if (fileItem==null)return;
-        @SuppressWarnings("rawtypes")
-        final AbstractRelations relations = (AbstractRelations) fileItem.getRelations();
-        final FileOntology fo = new FileOntology();
-        // relatie naar de dataset
-        relations.addRelation(
-                fo.isMetadataOn().get(), 
-                RelsConstants.getObjectURI(dataset.getStoreId()));
-        // type van de metadata
-        relations.addRelation(
-                fo.hasMetadataFormat().get(), 
-                FileOntology.MetadataFormat.DDM.toString(), 
-                RelsConstants.RDF_LITERAL);
+    public void addAdditionalRDF(final FileItem fileItem)
+    {
+        final String withoutExtension = fileItem.getPath().replaceAll("[.][^.]*$", "");
+        if (DDM.equals(withoutExtension) || EMD.equals(withoutExtension))
+        {
+            logger.info("connecting metadata file " + fileItem.getStoreId() + " with dataset " + dataset.getStoreId());
+            @SuppressWarnings("rawtypes")
+            final AbstractRelations relations = (AbstractRelations) fileItem.getRelations();
+            final FileOntology fo = new FileOntology();
+            final String datasetURI = RelsConstants.getObjectURI(dataset.getStoreId());
+            // relatie naar de dataset
+            relations.addRelation(fo.isMetadataOn().get(), datasetURI);
+            // type van de metadata
+            relations.addRelation(fo.hasMetadataFormat().get(), FO_DDM, RelsConstants.RDF_LITERAL);
+        }
     }
-
 }
