6395bf9fdeeb8cd685263d364c58f3dc3bbb022e
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2013-08-21 17:12:56 +0200

    Nieuwe EasyMailer naar easy-business verplaatst en configuratie Spring aangepast.

--- src/main/java/nl/knaw/dans/easy/web/mail/EasyMailerException.java
+++
@@ -1,9 +0,0 @@
-package nl.knaw.dans.easy.web.mail;
-
-public class EasyMailerException extends RuntimeException
-{
-    public EasyMailerException(String msg, Throwable cause)
-    {
-        super(msg, cause);
-    }
-}
