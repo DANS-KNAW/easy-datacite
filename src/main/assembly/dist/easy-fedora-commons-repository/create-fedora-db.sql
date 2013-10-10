20d6a9e2f1e09bbe816974857ef6924306b29b4f
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2013-10-10 16:57:13 +0200

    Bezig met installatie-handleiding

---
+++ src/main/assembly/dist/easy-fedora-commons-repository/create-fedora-db.sql
@@ -0,0 +1,2 @@
+CREATE ROLE "fedoraDbAdmin" LOGIN PASSWORD 'replace-with-save-password';
+CREATE DATABASE "fedora3" WITH ENCODING='UTF8' OWNER="fedoraAdmin";
---
+++ src/main/assembly/dist/util/java.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/java/default/
+export PATH=$PATH:$JAVA_HOME/bin
