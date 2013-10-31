7fdd260f4984dd150e5e247f918a80dc70bfea2b
joke.pol <joke.pol@dans.knaw.nl>
2013-10-31 13:45:52 +0100

    isSubmitted made public for propper error handling by sword

--- src/main/java/nl/knaw/dans/easy/business/dataset/DatasetSubmissionImpl.java
+++ src/main/java/nl/knaw/dans/easy/business/dataset/DatasetSubmissionImpl.java
@@ -35,7 +35,7 @@ public class DatasetSubmissionImpl implements DatasetSubmission
 
     /**
      * Construct a shallow copy for (limited) threaded use.
-     *
+     * 
      * @param submission
      *        submission to copy
      */
@@ -83,7 +83,7 @@ public class DatasetSubmissionImpl implements DatasetSubmission
         this.mailSend = mailSend;
     }
 
-    protected boolean isSubmitted()
+    public boolean isSubmitted()
     {
         return submitted;
     }
