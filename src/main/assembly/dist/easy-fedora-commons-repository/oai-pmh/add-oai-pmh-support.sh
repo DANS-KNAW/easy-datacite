8e73a2ef11e6940319bfdc4ef4a7f830b105cb2f
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2013-11-15 17:06:51 +0100

    OAI-PMH-support

--- src/main/assembly/dist/easy-fedora-commons-repository/oai-pmh/add-oai-pmh-support.sh
+++ src/main/assembly/dist/easy-fedora-commons-repository/oai-pmh/add-oai-pmh-support.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+#
+# Adds the files needed for OAI-PMH support.  Call with password of fedoraAdmin
+#
+
+
+fedora-ingest.sh f \ 
+				dans-xsl\:1.xml \ 
+				info:fedora/fedora-system:FOXML-1.1 \
+				localhost:8080 \
+				fedoraAdmin \ 
+				$1 \
+				http
+
+./add-datastream.sh dans-xsl:1 \
+					emd-mods.xsl \
+					"transformation:+emd+to+mods"\
+					fedoraAdmin \
+					$1 \
+					localhost \
+					8080			
+
 ./add-datastream.sh easy-sdep:oai-item1 \
 					EMD_carare.xsl \
 					"Stylesheet+to+convert+from+EMD+to+CARARE"\
