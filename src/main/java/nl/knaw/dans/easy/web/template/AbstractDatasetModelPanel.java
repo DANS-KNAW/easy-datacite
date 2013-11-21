df37549b3d3f8cf54e372fe2a6ae1521e27ace61
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2013-11-21 15:25:08 +0100

    Onnodige transient reference naar Dataset verwijderd.

--- src/main/java/nl/knaw/dans/easy/web/template/AbstractDatasetModelPanel.java
+++ src/main/java/nl/knaw/dans/easy/web/template/AbstractDatasetModelPanel.java
@@ -1,18 +1,11 @@
 package nl.knaw.dans.easy.web.template;
 
-import nl.knaw.dans.common.lang.service.exceptions.ServiceRuntimeException;
 import nl.knaw.dans.easy.domain.model.Dataset;
 import nl.knaw.dans.easy.web.common.DatasetModel;
 
-import org.apache.wicket.WicketRuntimeException;
-
-public abstract class AbstractDatasetModelPanel extends AbstractEasyPanel
+@SuppressWarnings("serial")
+public abstract class AbstractDatasetModelPanel extends AbstractEasyPanel<Dataset>
 {
-
-    private static final long serialVersionUID = -8845239111184791200L;
-
-    private transient Dataset dataset;
-
     public AbstractDatasetModelPanel(String wicketId, DatasetModel model)
     {
         super(wicketId, model);
@@ -20,18 +13,7 @@ public abstract class AbstractDatasetModelPanel extends AbstractEasyPanel
 
     protected Dataset getDataset()
     {
-        if (dataset == null)
-        {
-            try
-            {
-                dataset = (Dataset) getDefaultModelObject();
-            }
-            catch (ServiceRuntimeException e)
-            {
-                throw new WicketRuntimeException(e);
-            }
-        }
-        return dataset;
+       return  (Dataset) getDefaultModelObject();
     }
 
     protected DatasetModel getDatasetModel()
