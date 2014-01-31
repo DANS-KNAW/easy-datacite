626e47d87fdcdc90a32ae78dd6a1152cd3fee139
joke.pol <joke.pol@dans.knaw.nl>
2014-01-31 09:46:44 +0100

    puntjes op de i voor de messages

--- src/test/java/nl/knaw/dans/easy/web/authn/login/TestFederationToEasyAccountLinkingPage.java
+++ src/test/java/nl/knaw/dans/easy/web/authn/login/TestFederationToEasyAccountLinkingPage.java
@@ -1,8 +1,12 @@
 package nl.knaw.dans.easy.web.authn.login;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import nl.knaw.dans.easy.EasyWicketTester;
 
 import org.apache.wicket.Page;
+import org.apache.wicket.util.tester.FormTester;
 import org.apache.wicket.util.tester.ITestPageSource;
 import org.easymock.EasyMock;
 import org.junit.Before;
@@ -30,6 +34,8 @@ public class TestFederationToEasyAccountLinkingPage extends Fixture
     {
         tester = init();
         tester.dumpPage();
+
+        tester.assertRenderedPage(FederationToEasyAccountLinkingPage.class);
         tester.assertInvisible(COMMON_FEEDBACK);
         tester.assertInvisible(USER_FEEDBACK);
         tester.assertInvisible(CREDENTIALS_FEEDBACK);
@@ -45,10 +51,46 @@ public class TestFederationToEasyAccountLinkingPage extends Fixture
     {
         tester = init();
         tester.clickLink(REG_LOGIN_SUBMIT);
+        tester.dumpPage();
 
+        tester.assertRenderedPage(FederationToEasyAccountLinkingPage.class);
+        tester.assertLabelContains(COMMON_FEEDBACK, "Please check the fields indicated below.");
         tester.assertLabelContains(CREDENTIALS_FEEDBACK, "required");
         tester.assertLabelContains(USER_FEEDBACK, "required");
-        tester.assertLabelContains(COMMON_FEEDBACK, "Please check the fields indicated below.");
+    }
+
+    @Test
+    public void wrongUserOrPassordLogin() throws Exception
+    {
+        userService.authenticate(authentication);
+        EasyMock.expectLastCall().anyTimes();
+
+        // List<String> stateKeys = new ArrayList<String>();
+        // stateKeys.add("state.InvalidUsernameOrCredentials");
+        // EasyMock.expect(authentication.getAccumulatedStateKeys()).andStubReturn(stateKeys);
+        // EasyMock.expect(authentication.isCompleted()).andStubReturn(false);
+        // EasyMock.expect(authentication.getToken()).andStubReturn("mockedToken");
+        // EasyMock.expect(authentication.getUserId()).andStubReturn("mockedUserID");
+        // EasyMock.expect(authentication.getCredentials()).andStubReturn("mockedCredentials");
+        // authentication.setUserId(EasyMock.isA(String.class));
+        // EasyMock.expectLastCall().anyTimes();
+        // authentication.setCredentials(EasyMock.isA(String.class));
+        // EasyMock.expectLastCall().anyTimes();
+
+        tester = init();
+        FormTester formTester = tester.newFormTester("loginPanelRegular:loginForm");
+        formTester.setValue("userId", "wrongUser");
+        formTester.setValue("credentials", "wrongPassword");
+        tester.clickLink(REG_LOGIN_SUBMIT);
+        tester.dumpPage();
+        tester.assertRenderedPage(FederationToEasyAccountLinkingPage.class);
+
+        // manual online test is OK
+        // TODO refactor the Fixture approach after/with issue 671 show/delete federative link
+        // tester.assertLabelContains(COMMON_FEEDBACK,
+        // "This combination of username and password is not correct.");
+        tester.assertInvisible(USER_FEEDBACK);
+        tester.assertInvisible(CREDENTIALS_FEEDBACK);
     }
 
     protected EasyWicketTester init()
