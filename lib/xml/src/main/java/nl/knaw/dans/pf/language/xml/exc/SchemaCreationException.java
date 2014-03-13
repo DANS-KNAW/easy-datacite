6ed61696293aa724422e12852dce753e60a428ad
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2014-03-13 15:27:12 +0100

    - EASY Installation Guide bijgewerkt (minimale correcties) - Formattign.

--- lib/xml/src/main/java/nl/knaw/dans/pf/language/xml/exc/SchemaCreationException.java
+++ lib/xml/src/main/java/nl/knaw/dans/pf/language/xml/exc/SchemaCreationException.java
@@ -2,15 +2,15 @@ package nl.knaw.dans.pf.language.xml.exc;
 
 /**
  * Signals an exception in the creation of a {@link javax.xml.validation.Schema}.
+ * 
  * @author ecco
- *
  */
 public class SchemaCreationException extends XMLException
 {
 
     private static final long serialVersionUID = 1927611118541344925L;
 
-    // ecco (Sep 29, 2009): CHECKSTYLE: OFF 
+    // ecco (Sep 29, 2009): CHECKSTYLE: OFF
 
     public SchemaCreationException()
     {
