0d85f31d5716c08761ae223b02d7393bb86b676c
Heleen <heleen.van.de.schraaf@dans.knaw.nl>
2014-09-23 11:24:10 +0200

    EASY-798 Replace the original help-popup with a Bootstrap modal.
    Note: the popup is no longer draggable.
    
    Change-Id: I0a3c5debb3bf68611fc448f9b44569adf0a9ebc5

--- lib/dans-wicket/src/main/java/nl/knaw/dans/common/wicket/components/popup/HelpPopup.java
+++ lib/dans-wicket/src/main/java/nl/knaw/dans/common/wicket/components/popup/HelpPopup.java
@@ -1,9 +1,9 @@
 package nl.knaw.dans.common.wicket.components.popup;
 
-import org.apache.wicket.behavior.AbstractBehavior;
-import org.apache.wicket.markup.html.IHeaderResponse;
-import org.apache.wicket.markup.html.JavascriptPackageResource;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
 
 /**
@@ -12,34 +12,17 @@ import org.apache.wicket.markup.html.panel.Panel;
 public class HelpPopup extends Panel {
     private static final long serialVersionUID = -2271696165117255618L;
 
-    @SuppressWarnings("serial")
     public HelpPopup(String id, final String anchorName, final String content) {
         super(id);
-        add(JavascriptPackageResource.getHeaderContribution(HelpPopup.class, "HelpPopup.js"));
-        /*
-         * Warning: hackerisch code ahead! The label is filled with actual HTML-code!
-         */
-        add(new Label("popupHTML", createHtmlForPopup(anchorName, content)).setEscapeModelStrings(false));
 
-        add(new AbstractBehavior() {
-            public void renderHead(IHeaderResponse response) {
-                super.renderHead(response);
-                response.renderOnLoadJavascript("DANS.createHelpPopup('" + anchorName + "');");
-            }
-        });
-    }
-
-    private static String createHtmlForPopup(String anchorName, String content) {
-        return String.format( //
-                // Button/link to trigger pop-up
-                "<a href=\"#\" class='popupHelp' " //
-                        + "tabindex='-1' " //
-                        + "id='popupButton%s'>" //
-                        + "<img src='/images/button_help.gif' alt='help' /></a>\n" //
-
-                        // Initially hidden div-element that implements the dialog.
-                        + "<div id='popupDialog%s' class='yui-pe-content popupDialog' style='display:none'>\n" //
-                        + "<div class='hd'>Help</div>\n" //
-                        + "<div class='bd'>\n%s</div>\n</div>", anchorName, anchorName, content);
+        add(new ExternalLink("helpPopup", "#")
+            .add(new SimpleAttributeModifier("data-toggle", "modal"))
+            .add(new SimpleAttributeModifier("data-target", "#" + anchorName + "Modal"))
+        );
+        WebMarkupContainer popupModal = new WebMarkupContainer("popupModal");
+        popupModal.add(new SimpleAttributeModifier("id", anchorName + "Modal"))
+                  .add(new SimpleAttributeModifier("aria-labelledby", anchorName + "ModalLabel"));
+        popupModal.add(new Label("popupHTML", String.format("%s", content)).setEscapeModelStrings(false));
+        add(popupModal);
     }
 }
