ce01924883be3d15b7acb23de98eee67647502c6
Paul Boon <paul.boon@dans.knaw.nl>
2014-10-14 16:20:38 +0200

    EASY-804 Toevoegen Open Access aan licentiekeuze depositformulieren
    Voeg bij het veld "Access rights" als bovenste optie toe "Open access -
    unrestricted access." Keuze voor deze optie moet leiden tot de waarde
    OPEN_ACCESS in de EASY Metadata van de dataset
    
    https://drivenbydata.atlassian.net/browse/EASY-804
    
    fix formatting
    added OPEN_ACCESS to the license composer 'snippets'
    added license template for OpenAccess
    added open access as option to the deposit pages
    
    Change-Id: I0d4b6fa7821e62ca499516c4517751406c57a69e

--- lib/easy-business/src/main/java/nl/knaw/dans/easy/domain/dataset/item/filter/ItemFilter.java
+++ lib/easy-business/src/main/java/nl/knaw/dans/easy/domain/dataset/item/filter/ItemFilter.java
@@ -1,10 +1,8 @@
 package nl.knaw.dans.easy.domain.dataset.item.filter;
 
-
 /**
  * A filter object that can filter out file or folder items.
  * 
  * @author lobo
  */
-public interface ItemFilter {
-}
+public interface ItemFilter {}
