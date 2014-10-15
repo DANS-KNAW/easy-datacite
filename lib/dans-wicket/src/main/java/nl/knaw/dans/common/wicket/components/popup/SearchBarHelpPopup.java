03de2b5374337b3bf00dfc09efa8fc65f898fdb1
Heleen <heleen.van.de.schraaf@dans.knaw.nl>
2014-10-15 14:52:19 +0200

    EASY-730 Fix incorrect HelpPopup trigger icon.
    The help-popup trigger was incorrectly changed
    from a '?' image to 'Search help', because the
    help-popup is not specific for the search bar.
    
    Fix: create a subclass SearchBarHelpPopup which
    extends the HelpPopup HTML (which by default now
    has the original image) and replaces the '?' image
    with the text 'Search help'.
    
    Change-Id: I4b9d1fbf37a14cb2d6502b4ca231c809b9885b43

---
+++ lib/dans-wicket/src/main/java/nl/knaw/dans/common/wicket/components/popup/SearchBarHelpPopup.java
@@ -0,0 +1,10 @@
+package nl.knaw.dans.common.wicket.components.popup;
+
+public class SearchBarHelpPopup extends HelpPopup {
+
+    private static final long serialVersionUID = 1L;
+
+    public SearchBarHelpPopup(String id, String anchorName, String content) {
+        super(id, anchorName, content);
+    }
+}
