91c37fb1c1f3f9b8c69183f99d0d215155ecff35
Heleen <heleen.van.de.schraaf@dans.knaw.nl>
2015-01-16 15:36:03 +0100

    NO-JIRA-ISSUE Remove unused classes DatasetHitPanel & ViewCommonDatasetPage.
    Change-Id: Ice4aadefdbf7b7af0a84c5f53e53e34ee56d42d1

--- front-end/easy-webui/src/main/java/nl/knaw/dans/easy/web/search/EasySearchHitPanelFactory.java
+++ front-end/easy-webui/src/main/java/nl/knaw/dans/easy/web/search/EasySearchHitPanelFactory.java
@@ -1,6 +1,5 @@
 package nl.knaw.dans.easy.web.search;
 
-import nl.knaw.dans.common.lang.dataset.DatasetSB;
 import nl.knaw.dans.common.lang.search.SearchHit;
 import nl.knaw.dans.common.wicket.components.search.model.SearchModel;
 import nl.knaw.dans.common.wicket.components.search.results.SearchHitPanelFactory;
@@ -20,12 +19,8 @@ public class EasySearchHitPanelFactory implements SearchHitPanelFactory {
 
     @SuppressWarnings("unchecked")
     public Panel createHitPanel(String id, SearchHit<?> hit, SearchModel model) {
-        if (hit.getData() instanceof EasyDatasetSB) {
-            return new EasyDatasetHitPanel(id, new Model<SearchHit<EasyDatasetSB>>((SearchHit<EasyDatasetSB>) hit), model);
-        } else if (hit.getData() instanceof DatasetSB) {
-            return new DatasetHitPanel(id, new Model<SearchHit<DatasetSB>>((SearchHit<DatasetSB>) hit), model);
-        }
-        return null;
+        assert hit.getData() instanceof EasyDatasetSB: "hit.getData() must be instance of EasyDatasetSB";
+        return new EasyDatasetHitPanel(id, new Model<SearchHit<EasyDatasetSB>>((SearchHit<EasyDatasetSB>) hit), model);
     }
 
 }
