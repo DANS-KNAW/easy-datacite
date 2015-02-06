d3ee167607c49f0b31dbc5500109939f5e5bd97f
Heleen <heleen.van.de.schraaf@dans.knaw.nl>
2015-02-06 16:04:22 +0100

    EASY-730 Bootstrapify the Wicket modal windows.
    Mostly on the deposit detail page data files tab.
    
    Change-Id: I7b5c677d4966524cc02d0110bc40900dbf7df5ae

--- front-end/easy-webui/src/main/java/nl/knaw/dans/easy/web/common/StyledModalWindow.java
+++ front-end/easy-webui/src/main/java/nl/knaw/dans/easy/web/common/StyledModalWindow.java
@@ -2,6 +2,7 @@ package nl.knaw.dans.easy.web.common;
 
 import nl.knaw.dans.easy.web.template.Style;
 
+import org.apache.wicket.ResourceReference;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 
 public class StyledModalWindow extends ModalWindow {
@@ -9,27 +10,31 @@ public class StyledModalWindow extends ModalWindow {
 
     public StyledModalWindow(String id) {
         super(id);
-        add(Style.MODAL_HEADER_CONTRIBUTION);
     }
 
     public StyledModalWindow(String id, String title) {
         super(id);
-        add(Style.MODAL_HEADER_CONTRIBUTION);
         setTitle(title);
     }
 
     public StyledModalWindow(String id, int initialWidth) {
         super(id);
-        add(Style.MODAL_HEADER_CONTRIBUTION);
         setUseInitialHeight(false);
         setInitialWidth(initialWidth);
     }
 
     public StyledModalWindow(String id, String title, int initialWidth) {
         super(id);
-        add(Style.MODAL_HEADER_CONTRIBUTION);
         setTitle(title);
         setUseInitialHeight(false);
         setInitialWidth(initialWidth);
+    }
+
+    @Override
+    protected ResourceReference newCssResource() {
+        // This causes the default Wicket CSS not to be loaded without having to
+        // use our own specific CSS file.
+        // We've included the new styles in the overall project CSS file.
+        return null;
     }
 }
