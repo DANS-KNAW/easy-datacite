c16d7bc4bddfc8d395ffee05ca64122c441c9087
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2015-04-30 16:08:55 +0200

    Opschonen code n.a.v. EASY-866: verwijder dans-pid-project.
    Change-Id: I3d5ef3a8d2acc394ffba995e4621c9cb094efc99

--- lib/dans-pid/sql/create_db.sql
+++
@@ -1,8 +0,0 @@
--- Database: pid_generator
-
-CREATE DATABASE pid_generator
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       LC_COLLATE = 'English, United States'
-       LC_CTYPE = 'English, United States'
-       CONNECTION LIMIT = -1;
