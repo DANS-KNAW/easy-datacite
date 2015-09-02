d113dd29ef762f2c46de52f887eb359969fdedec
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2015-09-02 17:31:31 +0200

    easy-dtap variabelen toegevoegd aan application.properties en setting.sh
    Change-Id: I8a1a6aeb2d0406b7c441701fd15cbb0e659e8ffd

--- tool/task-dump-metadata/src/main/assembly/dist/cfg/settings.sh
+++ tool/task-dump-metadata/src/main/assembly/dist/cfg/settings.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 # Define the following variables in this file:
 # export KEY_FOR_TARGET_SERVER=<location of key for non-interactive login to target server>
-# export TARGET_URL=<url to copy the zipped export to>
-# export ERROR_MAIL_RECIPIENT=<E-mail address to mail error messages to>
+export TARGET_URL="{{ task_dump_md_target_path }}"
+export ERROR_MAIL_RECIPIENT="{{ task_dump_md_error_mail_recipient }}"
