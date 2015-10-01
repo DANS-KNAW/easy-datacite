83b76467e3e3ea7e17fa55893209976daed9d5f5
joke.pol <joke.pol@dans.knaw.nl>
2015-10-01 16:31:25 +0200

    EASY-918 PID generator werkt pas na herstart
    - @tailrec uitzetten vanwege de volgende error tijdens maven build:could
    not optimize @tailrec annotated method iterWhileRestarting: it contains
    a recursive call not in tail position
    Ik heb geen idee hoe ik dat oplossen.
    - weghalen van SocketException want dat gebeurd niet altijd.
    
    Change-Id: I236718e77d0a0d90e251be9204c4bb84319bb7d6

--- back-end/easy-pid-generator/src/main/scala/nl/knaw/dans/easy/pid/SeedStorage.scala
+++ back-end/easy-pid-generator/src/main/scala/nl/knaw/dans/easy/pid/SeedStorage.scala
@@ -1,17 +1,21 @@
 package nl.knaw.dans.easy.pid
 
 import java.io.File
-import org.apache.commons.io.FileUtils._
-import scala.util.Try
-import java.io.IOException
-import scala.util.Success
-import scala.util.Failure
-import org.hibernate.cfg.Configuration
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder
-import org.hibernate.HibernateException
+import java.lang.Thread._
+import java.net.SocketException
 import javax.persistence.Entity
-import scala.beans.BeanProperty
+
+import org.hibernate.HibernateException
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder
+import org.hibernate.cfg.Configuration
+import org.hibernate.exception.JDBCConnectionException
+import org.hibernate.exception.GenericJDBCException
+import org.postgresql.util.PSQLException
 import org.slf4j.LoggerFactory
+
+import scala.annotation.tailrec
+import scala.beans.BeanProperty
+import scala.util.{Failure, Success, Try}
 
 sealed trait SeedStorage {
   /**
@@ -35,43 +39,59 @@ case class RanOutOfSeeds() extends Exception
 
 case class DbBasedSeedStorage(key: String, first: Long, hibernateConfig: File) extends SeedStorage {
   val log = LoggerFactory.getLogger(classOf[DbBasedSeedStorage])
-  val conf = new Configuration().configure(hibernateConfig);
-  val regBuilder = new StandardServiceRegistryBuilder()
-  regBuilder.applySettings(conf.getProperties());
-  val serviceRegistry = regBuilder.build();
-  val sessionFactory = conf.buildSessionFactory(serviceRegistry);
+  val conf = new Configuration().configure(hibernateConfig)
+  val serviceRegistry = new StandardServiceRegistryBuilder().applySettings(conf.getProperties).build()
+
+  var sessionFactory = conf.buildSessionFactory(serviceRegistry)
 
   override def calculateAndPersist(f: Long => Option[Long]): Try[Long] = {
-    val session = sessionFactory.getCurrentSession
-    session.beginTransaction()
-    try {
-      session.get(classOf[Seed], key) match {
-        case seed: Seed =>
-          f(seed.value) match {
-            case Some(next) =>
-              val seed = new Seed
-              seed.pidType = key
-              seed.value = next
-              session.merge(seed)
-              session.getTransaction.commit()
-              Success(next)
-            case None => Failure(RanOutOfSeeds())
+
+    //@tailrec
+    def iterWhileRestarting(timeout: Int = 0, maxRetry: Int = 3): Try[Long] = {
+      val session = sessionFactory.getCurrentSession
+      session.beginTransaction()
+      try {
+        session.get(classOf[Seed], key) match {
+          case seed: Seed =>
+            f(seed.value) match {
+              case Some(next) =>
+                val seed = new Seed
+                seed.pidType = key
+                seed.value = next
+                session.merge(seed)
+                session.getTransaction.commit()
+                Success(next)
+              case None => Failure(RanOutOfSeeds())
+            }
+          case _ =>
+            log.warn("NO PREVIOUS PID FOUND. THIS SHOULD ONLY HAPPEN ONCE!! INITIALIZING WITH INITIAL SEED FOR {}", key)
+            log.info("Initializing seed with value {}", first)
+            val seed = new Seed
+            seed.pidType = key
+            seed.value = first
+            session.save(seed)
+            session.getTransaction.commit()
+            Success(first)
+        }
+      } catch {
+        case e: GenericJDBCException if e.getCause.isInstanceOf[PSQLException] =>
+          val msg = s"""Database server connection lost
+                       |GenericJDBCException ${e.getMessage}
+                       |PSQLException ${e.getCause.getMessage}}""".stripMargin
+          if (maxRetry <= 0 ) Failure (new RuntimeException(msg))
+          else {
+            log.warn(s"Trying with a new session factory, $msg")
+            sessionFactory = conf.buildSessionFactory(serviceRegistry)
+            if (timeout > 0)
+              sleep(timeout)
+            iterWhileRestarting(5000, maxRetry-1)
           }
-        case _ =>
-          log.warn("NO PREVIOUS PID FOUND. THIS SHOULD ONLY HAPPEN ONCE!! INITIALIZING WITH INITIAL SEED FOR {}", key)
-          log.info("Initializing seed with value {}", first)
-          val seed = new Seed
-          seed.pidType = key
-          seed.value = first
-          session.save(seed)
-          session.getTransaction.commit()
-          Success(first)
+        case e: HibernateException =>
+          log.error("Database error", e)
+          session.getTransaction.rollback()
+          Failure(e)
       }
-    } catch {
-      case e: HibernateException =>
-        log.error("Database error", e)
-        session.getTransaction.rollback()
-        Failure(e)
     }
+    iterWhileRestarting()
   }
 }
