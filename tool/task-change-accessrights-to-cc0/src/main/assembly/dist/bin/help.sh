cc49eb84cc83fa9f1f8c0ed1a2288333f4129407
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2017-01-26 14:13:20 +0100

    Branch 2.16.1 prepare for EASY-1003
    Prepare the release 2.16.1
    
    
    * Rename task-change-synthegra-to-cc0 to task-change-accessrights-to-cc0
    * Create the module task-change-accessrights-to-cc0 so it will be build
    * Create the module task-add-new-license so it will be build
    * Rename the task-add-new-license variables to follow the same pattern as other tasks
    
    Although the 'Synthegra'-usecase was the direct trigger for the development of this task, it is no longer the only usecase that will require this. Therefor we opted for a more generic name.

--- tool/task-change-accessrights-to-cc0/src/main/assembly/dist/bin/help.sh
+++ tool/task-change-accessrights-to-cc0/src/main/assembly/dist/bin/help.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 pwd
+
 java -Dlogback.configurationFile=cfg/logback.xml \
-     -cp bin/task-change-synthegra-to-cc0.jar \
-     nl.knaw.dans.easy.task.Main $@
+     -jar bin/task-change-accessrights-to-cc0.jar \
+     --help
