417b605982364a434d4898d46ac9e37684cafaad
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2017-06-10 11:41:04 +0200

    Changed ansible variable names

--- front-end/easy-proai/src/main/assembly/dist/install/create-proai-db.sql
+++ front-end/easy-proai/src/main/assembly/dist/install/create-proai-db.sql
@@ -1,6 +1,6 @@
-CREATE ROLE {{ proai_db_admin_username }} WITH LOGIN PASSWORD '{{ proai_db_admin_password }}';
+CREATE ROLE {{ easy_proai_db_admin_username }} WITH LOGIN PASSWORD '{{ easy_proai_db_admin_password }}';
 
 CREATE DATABASE proai_db
-  WITH OWNER = {{ proai_db_admin_username }}
+  WITH OWNER = {{ easy_proai_db_admin_username }}
        ENCODING = 'UTF8'
        CONNECTION LIMIT = -1;
